03f91eadd8968fdbd2256cd644f610881c961ede
Laiba Hameed <laibarain2@gmail.com>
2026-09-12 07:51:48 -0700

    Add files via upload

---
+++ function/app.js
@@ -0,0 +1,10 @@
+function booksTotal(){
+    let book1 = 1000;
+    let book2 = 1500;
+    return book1 + book2;
+}
+console.log("Total price:", booksTotal());
+for (let i=1; i <=3; i++){ 
+    console.log("Book:", i);
+}
+    
